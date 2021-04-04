b2248a5d754692a5dd17cf4529c29d52b61d92b7
Kadek Surya Mahardika <kadeksuryam@gmail.com>
2021-04-05 00:05:30 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var mapInfo
+
+function loadFile() {
+  var input, file, fr;
+
+  if (typeof window.FileReader !== 'function') {
+    alert("The file API isn't supported on this browser yet.");
+    return;
+  }
+
+  input = document.getElementById('fileinput');
+  if (!input) {
+    alert("Um, couldn't find the fileinput element.");
+  }
+  else if (!input.files) {
+    alert("This browser doesn't seem to support the `files` property of file inputs.");
+  }
+  else if (!input.files[0]) {
+    alert("Please select a file before clicking 'Load'");
+  }
+  else {
+    file = input.files[0];
+    fr = new FileReader();
+    fr.onload = receivedText;
+    fr.readAsText(file);
+  }
+
+  function receivedText(e) {
+    let lines = e.target.result;
+    console.log(lines);
+    mapInfo = JSON.parse(lines.toString()); 
+    //console.log(newArr);
+    var map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([6, 6]),
+        zoom: 4
+      })
+    });
+  }
+}
